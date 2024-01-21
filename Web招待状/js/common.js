d8f437fa729a95849f421f8a0c093b0a559c7eff
ozawa-shuhei <128103975+ozawa-shuhei@users.noreply.github.com>
2024-01-21 15:01:53 +0900

    001

---
+++ Web招待状/js/common.js
@@ -0,0 +1,25 @@
+$(window).on('load',function(){
+    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  });
+
+$('.slider').slick({
+    autoplay:true,
+    fade:true,
+    arrows: false,
+});
+
+$(window).scroll(function () {
+    var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right');
+    var scrollAnimationFunc = function () {
+      for (var i = 0; i < scrollAnimationElm.length; i++) {
+        var triggerMargin = 100;
+        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+          scrollAnimationElm[i].classList.add('on');
+        }
+      }
+    }
+    window.addEventListener('load', scrollAnimationFunc);
+    window.addEventListener('scroll', scrollAnimationFunc);
+  });
+
